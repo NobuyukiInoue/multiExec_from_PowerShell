8d2f8bcbd922e105997dcc42b5342acf806ecc1d
nobuyuki inoue <spring555@gmail.com>
2018-06-02 13:37:57 +0900

    modify ReadKey

--- BAT/snmpwalk_13.231.255.196_IFMIB.bat
+++
@@ -1,1 +0,0 @@
-snmpwalk -v 1 -c public 13.231.255.196 .1.3.6.1.2.1.2 > D:\log\13.231.255.196_IFMIB.log
--- BAT/snmpwalk_13.231.255.196_ifAdminStatus.bat
+++
@@ -1,1 +0,0 @@
-snmpwalk -v 1 -c public 13.231.255.196 .1.3.6.1.2.1.2.2.1.7 > D:\log\13.231.255.196_ifAdminStatus.log
